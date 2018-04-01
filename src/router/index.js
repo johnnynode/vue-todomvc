eedc4b57fb8ad30b938410797bf655b6e3e59431
johnnynode <johnnynode@gmail.com>
2018-04-01 18:25:08 +0800

    add Hi component test

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Hi from '@/components/Hi'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Hi',
+      name: 'Hi',
+      component: Hi
     }
   ]
 })
